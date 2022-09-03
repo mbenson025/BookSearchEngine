73fe9fd38941301fb302cd0cbb1c88f62650d914
mbenson025 <mbenson025@gmail.com>
2022-09-03 09:52:47 -0500

    revert connection.js file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,17 +1,11 @@
 const mongoose = require('mongoose');
 
-module.exports = {
-  start: async function () {
-    await mongoose.connect(
-      process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        // useFindAndModify: false,
-      }
-    );
-    await mongoose.set('debug', true);
-  },
-};
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
-// module.exports = mongoose.connection;
+module.exports = mongoose.connection;
