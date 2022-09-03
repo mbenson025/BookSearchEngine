ef93be14ba37d52e07858bd35cc07ff2801466e8
mbenson025 <mbenson025@gmail.com>
2022-09-02 23:23:37 -0500

    test

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.connect(
-  mongoose.connect(
-    `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@booksearchengine.hk6ucvm.mongodb.net/?retryWrites=true&w=majorityretry`
-  ) || 'mongodb://localhost:27017/googlebooks',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
